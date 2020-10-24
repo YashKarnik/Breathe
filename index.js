0e1384f4e41272d627d2faa496620960b20b7680
Yash Karnik <55018090+YashKarnik@users.noreply.github.com>
2020-10-24 17:08:16 +0530

    fix javascript

--- index.js
+++ index.js
@@ -1,16 +1,8 @@
-// let containerRadiusBefore = (containerRadiusAfter = 0);
-// let isIncreasing;
-// let container = document.querySelector('.container');
-// let headingText = document.querySelector('.text');
-
-// function UpdateRadiusHook() {
-// 	containerRadiusBefore = container.offsetWidth;
-// 	setTimeout(() => {
-// 		containerRadiusAfter = container.offsetWidth;
-// 		if (containerRadiusBefore < containerRadiusAfter) isIncreasing = true;
-// 		else isIncreasing = false;
-// 	}, 1000);
-// 	console.log({ containerRadiusAfter, containerRadiusBefore });
-// 	requestAnimationFrame(UpdateRadiusHook);
-// }
-// requestAnimationFrame(UpdateRadiusHook);
+let container = document.querySelector('.container');
+let headingText = document.querySelector('.text');
+let Flag = false;
+let totalTime = 8000;
+setInterval(() => {
+	headingText.innerHTML = Flag ? 'INHALE' : 'EXHALE';
+	Flag = !Flag;
+}, totalTime);
