9e293e4e789e77644ef53f2ca06d758834fd599c
Yash Karnik <55018090+YashKarnik@users.noreply.github.com>
2020-10-24 17:55:11 +0530

    fix directory structure

--- index.js
+++
@@ -1,8 +0,0 @@
-let container = document.querySelector('.container');
-let headingText = document.querySelector('.text');
-let Flag = false;
-let totalTime = 8000;
-setInterval(() => {
-	headingText.innerHTML = Flag ? 'INHALE' : 'EXHALE';
-	Flag = !Flag;
-}, totalTime);
